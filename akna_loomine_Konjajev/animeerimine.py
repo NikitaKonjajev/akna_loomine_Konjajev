4469560b922845ea74a8ef42227373cb630996bf
Никита <@DESKTOP-HK185OH>
2023-04-19 21:32:32 +0300

    1234

--- akna_loomine_Konjajev/animeerimine.py
+++ akna_loomine_Konjajev/animeerimine.py
@@ -1,5 +1,6 @@
 import pygame,sys
 from def12 import *
+
 pygame.init()
 #värvid
 kollane=[255,255,10]
@@ -18,35 +19,46 @@ kell=pygame.time.Clock()
 mesilane=pygame.image.load("stiv2.png")
 posX=X-mesilane.get_rect().width
 posY=Y-mesilane.get_rect().height
-
 lõpp=False
 
-sammX=0
-sammY=0
-while not lõpp:
-    kell.tick(60)
-    event=pygame.event.get()
-    for i in pygame.event.get():
-        if i.type==pygame.QUIT():
-            sys.exit()
-    posX,posY,sammX,sammY=anim(posX,posY,sammX,sammY,X,Y,mesilane)
-    ekraan.blit(mesilane,(posX,posY))
-    pygame.display.flip()
-    ekraan.fill(roheline)
+while True:
+    valik=input("Vali pildi number  1 voi 2: ")
+    if valik=="1":
+        sammX=0
+        sammY=0
+        while not lõpp:
+            kell.tick(60)
+            event=pygame.event.get()
+            for i in pygame.event.get():
+                if i.type==pygame.QUIT():
+                    sys.exit()
+            posX,posY,sammX,sammY=anim(posX,posY,sammX,sammY,X,Y,mesilane)
+            ekraan.blit(mesilane,(posX,posY))
+            pygame.display.flip()
+            ekraan.fill(roheline)
+        pygame.quit()
+        break
+    elif valik=="2":
+        sammX=2
+        sammY=2
+        while not lõpp:
+            kell.tick(60)
+            event = pygame.event.get()
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT():
+                    sys.exit()
+            ekraan.blit(mesilane, (posX, posY))
+            posX,posY,sammX,sammY=anim2(posX,posY,sammX,sammY,X,Y,mesilane)
+            pygame.display.flip()
+            ekraan.fill(roheline)
+        pygame.quit()
+        break
+    else:
+        print("Sa valisid ebakorrektse väärtuse")
 
-sammX=2
-sammY=2
-while not lõpp:
-    kell.tick(60)
-    event = pygame.event.get()
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT():
-            sys.exit()
-    ekraan.blit(mesilane, (posX, posY))
-    posX,posY,sammX,sammY=anim2(posX,posY,sammX,sammY,X,Y,mesilane)
-    pygame.display.flip()
-    ekraan.fill(roheline)
-pygame.quit()
+
+
+
 
 """ мой код, который почти работал,чтобы она катался по кругу
 while not lõpp:
@@ -71,31 +83,3 @@ while not lõpp:
     posX-=sammX
     posY-=sammY
 """
-def anim(posX, posY, sammX, sammY, X, Y, mesilane): #сам код из кода Марины
-    if posX == 0 and posY == 0:
-        sammX = 1
-        sammY = 0
-    if posX == X - mesilane.get_rect().width and posY <= Y - mesilane.get_rect().height:
-        sammX = 0
-        sammY = 1
-    if posX <= X - mesilane.get_rect().width and posY == Y - mesilane.get_rect().height:
-        sammX = 1
-        sammY = 0
-        sammX = -sammX
-    if posX == 0 and posY >= Y - mesilane.get_rect().height:
-        sammX = 0
-        sammY = 1
-        sammY = -sammY
-    posX += sammX
-    posY += sammY
-    return posX, posY, sammX, sammY
-
-
-def anim2(posX, posY, sammX, sammY, X, Y, mesilane):
-    posX -= sammX
-    posY -= sammY
-    if posX >= X - mesilane.get_rect().width or posX < 0:
-        sammX = -sammX
-    if posY >= Y - mesilane.get_rect().height or posY < 0:
-        sammY = -sammY
-    return posX, posY, sammX, sammY
