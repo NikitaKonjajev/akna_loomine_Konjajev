46629b18d98e15a9c6155429ffa6e6138e3861cf
opilane <opilane@HP-CZC2349HV1>
2023-05-02 13:12:29 +0300

    рабочий счет

--- akna_loomine_Konjajev/pingpong.py
+++ akna_loomine_Konjajev/pingpong.py
@@ -1,5 +1,6 @@
 ﻿import pygame, random
-pygame. init()
+pygame.init()
+
 
 red = [255, 0, 0]
 green = [0, 255, 0]
@@ -7,15 +8,18 @@ blue = [6, 6, 255]
 pink = [255, 153, 255]
 lGreen = [153, 255, 153]
 lBlue = [153, 204, 255]
+
 screenX = 640
 screenY = 480
-screen=pygame.display. set_mode([screenX, screenY])
+screen=pygame.display.set_mode([screenX, screenY])
 pygame.display.set_caption("pingpong")
 screen.fill(lBlue)
 clock=pygame.time.Clock()
 posX,posY=0,0
 speedX,speedY=3,4
 
+def my(score):
+    value=screen.render("Vash s4et: "+str(score),True)dis.blit(value,[0,0])
 #player
 player = pygame.Rect(posX, posY, 120, 120)
 playerImage = pygame.image.load("sarik3.png")
@@ -28,48 +32,51 @@ totalenemies = 20
 score = 0
 gameover=False
 while not gameover:
-    clock. tick(60)
+    clock.tick(60)
     #mangu sulgemine ristist
     event = pygame.event.poll()
     if event.type==pygame.QUIT:
         break
     player = pygame.Rect(posX, posY, 120, 140)
-    screen. blit(playerImage, player)
+    screen.blit(playerImage, player)
+    
     #левый борт
     pilt=pygame.image.load("bortik1.png")
     screen.blit(pilt,(150,-80))
-    pygame.display.flip()
+    
     #правый борт
+    pilt=pygame.image.load("bortik1.png")
+    screen.blit(pilt,(150,368))
 
+    pygame.display.flip()
 
-    jalg=pygame.Rect(490,200,30,200)
-    pygame.draw.rect(screen,(139, 69, 19),jalg)
-
-
-
-
-    posX+=speedX
-    posY+=speedY
-
-    if posX > screenX-playerImage.get_rect().width or posX < 0:
+    # Check collision with borders
+    if posX > screenX - playerImage.get_rect().width or posX < 0:
         speedX = -speedX
-
-    if posY > screenY-playerImage.get_rect().height or posY < 0:
+        if posY > 150 and posY < 368:
+            score += 1
+        print(score)
+    
+    if posY > screenY - playerImage.get_rect().height or posY < 0:
         speedY = -speedY
+        if posX > 150 and posX < 290:
+            score += 1
+        print(score)
+
+    # Update ball position
+    posX += speedX
+    posY += speedY
 
     for enemy in enemies[:]:
-            if player.colliderect(enemy):
-                enemies.remove(enemy)
-                score += 1
+        if player.colliderect(enemy):
+            enemies.remove(enemy)
+            score += 1
             print(score)
 
-    for enemy in enemies:
-        screen.blit(enemyImage,enemy)
-
     pygame.display.flip()
-    screen. fill(lBlue)
+    screen.fill(lBlue)
 
 print(score)
-if score==20:
+if score == 20:
     gameover = True
-pygame. quit
+pygame.quit()
